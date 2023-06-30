b969d13abf4000810dc43db616f331f6d2e129f9
Adam.Zieba <oldcafeots@gmail.com>
2023-06-30 16:56:09 +0200

    Add generateMeasurements module and main module

---
+++ sample/generateMeasurements.py
@@ -0,0 +1,44 @@
+
+import random
+
+# Generate measurements
+
+def generate_json_measurements():
+    pass
+
+
+def generate_pm1_0cf1():
+    return "Generated pm1_0cf1 measurement"
+
+def generate_pm2_5cf1():
+    return "Generated pm2_5cf1 measurement"
+
+def generate_pm10cf1():
+    return "Generated pm10cf1 measurement"
+
+def generate_pm1_0():
+    return "Generated pm1_0 measurement"
+
+def generate_pm2_5():
+    return "Generated pm2_5 measurement"
+
+def generate_pm10():
+    return "Generated pm10 measurement"
+
+def generate_n0_3():
+    return "Generated n0_3 measurement"
+
+def generate_n0_5():
+    return "Generated n0_5 measurement"
+
+def generate_n1_0():
+    return "Generated n1_0 measurement"
+
+def generate_n2_5():
+    return "Generated n2_5 measurement"
+
+def generate_n5_0():
+    return "Generated n5_0 meaurement"
+
+def generate_n10():
+    return "Generated n10 measurement"
---
+++ sample/main.py
@@ -0,0 +1,5 @@
+import generateMeasurements
+
+
+pm1_generated = generateMeasurements.generate_pm1_0cf1()
+print(pm1_generated)
