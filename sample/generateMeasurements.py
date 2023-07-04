4de351032084e4f3dc0a364a43f70236e3164168
Adam.Zieba <oldcafeots@gmail.com>
2023-07-04 16:50:04 +0200

    Add JSON parsing

--- sample/generateMeasurements.py
+++ sample/generateMeasurements.py
@@ -1,12 +1,25 @@
 
 import random
+import json
 
 # Generate measurements
 
 def generate_json_measurements():
-    return {
-        'pm1_0cf1': generate_pm10cf1()
+    json_format = {
+        'pm1_0cf1': generate_pm10cf1(),
+        'pm2_5cf1': generate_pm2_5cf1(),
+        'pm10cf1': generate_pm10cf1(),
+        'pm1_0': generate_pm1_0cf1(),
+        'pm2_5': generate_pm2_5(),
+        'pm10': generate_pm10(),
+        'n0_3': generate_n0_3(),
+        'n0_5': generate_n5_0(),
+        'n1_0': generate_n1_0(),
+        'n2_5': generate_n2_5(),
+        'n5_0': generate_n5_0(),
+        'n10': generate_n10()
     }
+    return json.dumps(json_format)
 
 
 def generate_pm1_0cf1():
--- sample/main.py
+++ sample/main.py
@@ -1,32 +1,4 @@
 import generateMeasurements
 
-'''
-pm1_0cf1_generated = generateMeasurements.generate_pm1_0cf1()
-pm2_5cf1_generated = generateMeasurements.generate_pm2_5cf1()
-pm10_cf1_generated = generateMeasurements.generate_pm10cf1()
-pm1_0_generated = generateMeasurements.generate_pm1_0()
-pm2_5_generated = generateMeasurements.generate_pm2_5()
-pm10_generated = generateMeasurements.generate_pm10()
-pm_n0_3_generated = generateMeasurements.generate_n0_3()
-pm_n0_5_generated = generateMeasurements.generate_n0_5()
-pm_n1_0_generated = generateMeasurements.generate_n1_0()
-pm_n2_5_generated = generateMeasurements.generate_n2_5()
-pm_n5_0_generated = generateMeasurements.generate_n5_0()
-pm_n10_generated = generateMeasurements.generate_n10()
-
-print(pm1_0cf1_generated)
-print(pm2_5cf1_generated)
-print(pm10_cf1_generated)
-print(pm1_0_generated)
-print(pm2_5_generated)
-print(pm10_generated)
-print(pm_n0_3_generated)
-print(pm_n0_5_generated)
-print(pm_n1_0_generated)
-print(pm_n2_5_generated)
-print(pm_n5_0_generated)
-print(pm_n10_generated)
-'''
-
-json_returned = generateMeasurements.generate_json_measurements()
-print(json_returned)
+json_readings= generateMeasurements.generate_json_measurements()
+print(json_readings)
